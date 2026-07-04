9d74ebbd90f8c66f12458d0debf99fb6a77b7e4e
fahimullah-67 <fahimullah446@gmail.com>
2026-07-05 01:28:13 +0500

    Initial commit

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,13 @@ connectDB()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin: process.env.VITE_API_BASE_URL,
+    credentials: true
+  }
+)
+)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Silk Task API is running' })
